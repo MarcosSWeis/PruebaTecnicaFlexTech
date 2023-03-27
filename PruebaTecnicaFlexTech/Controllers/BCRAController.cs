6cf4d0ae0732476971da0d8581c363f01f7f655e
Marcos Weis <91438906+MarcosSWeis@users.noreply.github.com>
2023-03-27 14:05:16 -0300

    Update BCRAController.cs

--- PruebaTecnicaFlexTech/Controllers/BCRAController.cs
+++ PruebaTecnicaFlexTech/Controllers/BCRAController.cs
@@ -18,7 +18,7 @@ namespace PruebaTecnicaFlexTech.Controllers
             return Ok(new { fecha = date,informacion = information.ToUpper() });
         }
 
-        //Agrego esta forma proque me parecio una forma mas orderna de plantear el ejercicio, haciendo mencion a lo que hablaos de clases
+        //Agrego esta forma proque me parecio una forma mas orderna de plantear el ejercicio, haciendo mencion a lo que hablamos de clases
         //si bien en le otro metodo uso una clase anonima que me devuelve los mismo un string fecha y el string del input en mayuscula
         //en este metodo contamos con el tipo de dato que devuelve eso parametros (InformationJson) y podemos controlar lo que devolvemos
 
