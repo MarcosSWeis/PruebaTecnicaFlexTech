36a44c0c87733733a525c4321592cdbc728f3da0
MarcosSWeis <marcosweis01@gmail.com>
2023-03-27 14:01:22 -0300

    otro enfoque al problema, mas enfocado a lo que hablamos en la charla sobre las clase

--- PruebaTecnicaFlexTech/Controllers/BCRAController.cs
+++ PruebaTecnicaFlexTech/Controllers/BCRAController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PruebaTecnicaFlexTech.Models;
 
 namespace PruebaTecnicaFlexTech.Controllers
 {
@@ -10,11 +11,21 @@ namespace PruebaTecnicaFlexTech.Controllers
 
 
         [HttpGet("{information}")]
-        public ActionResult GetJson(string information)
+        public IActionResult GetJson(string information)
         {
             var date = DateTime.Now.ToString("dd/MM/yyyy");
 
             return Ok(new { fecha = date,informacion = information.ToUpper() });
+        }
+
+        //Agrego esta forma proque me parecio una forma mas orderna de plantear el ejercicio, haciendo mencion a lo que hablaos de clases
+        //si bien en le otro metodo uso una clase anonima que me devuelve los mismo un string fecha y el string del input en mayuscula
+        //en este metodo contamos con el tipo de dato que devuelve eso parametros (InformationJson) y podemos controlar lo que devolvemos        
+        [HttpGet("{information}")]
+        public InformationJson GetJsonRefactor(string information)
+        {
+            InformationJson info = new(DateTime.Now,information);
+            return info;
         }
     }
 }
---
+++ PruebaTecnicaFlexTech/Models/InformationJson.cs
@@ -0,0 +1,20 @@
+﻿namespace PruebaTecnicaFlexTech.Models
+{
+    public class InformationJson
+    {
+        public InformationJson(DateTime fecha,string information)
+        {
+            Fecha = ParseDate(fecha);
+            Information = information;
+        }
+        public string Fecha { get; set; }
+
+        public string Information { get; set; }
+        private string ParseDate(DateTime date)
+        {
+
+            return date.ToString("dd/MM/yyyy");
+        }
+
+    }
+}
