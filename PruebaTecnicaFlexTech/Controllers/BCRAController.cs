18881dddee89cff608f16bb24b9a6fda4b29cf3f
MarcosSWeis <marcosweis01@gmail.com>
2023-03-27 11:48:07 -0300

    Add project files.

---
+++ PruebaTecnicaFlexTech/Controllers/BCRAController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaTecnicaFlexTech.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BCRAController :ControllerBase
+    {
+
+
+        [HttpGet("{information}")]
+        public ActionResult GetJson(string information)
+        {
+            var date = DateTime.Now.ToString("dd/MM/yyyy");
+
+            return Ok(new { fecha = date,informacion = information.ToUpper() });
+        }
+    }
+}
